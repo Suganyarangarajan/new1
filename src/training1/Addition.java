c2bf9f4320941177be7b9bc606a58a9b7796c59c
mmani <mmani@54VMJR2>
2022-02-23 17:31:04 -0500

    Subtraction program added

---
+++ src/training1/Addition.java
@@ -0,0 +1,13 @@
+package training1;
+
+
+
+public class Addition {
+	
+	public static void main(String[] args)
+	{
+		
+		System.out.println("a+b is");
+	}
+
+}
---
+++ src/training1/Subtraction.java
@@ -0,0 +1,10 @@
+package training1;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+	System.out.println("Subtraction = "+(20-10));
+
+	}
+
+}
